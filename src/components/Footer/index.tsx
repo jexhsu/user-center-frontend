8262550b3e1ef2edb12293115e62d3b361a0acc2
JexHsu <jexhsu@gmail.com>
2024-08-27 12:25:16 +0800

    🎨 refactor(Footer): update footer links and copyright

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,35 +1,26 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
-import React from 'react';
 
 const Footer: React.FC = () => {
+  const defaultMessage = 'JexHsu';
+  const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
-      style={{
-        background: 'none',
-      }}
+      // @ts-ignore
+      copyright={
+        <>
+          {currentYear} {defaultMessage}
+        </>
+      }
       links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          href: 'https://github.com/jexhsu',
           blankTarget: true,
         },
       ]}
     />
   );
 };
-
 export default Footer;
